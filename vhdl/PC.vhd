9c446477588971b99f7d5f28514b8256ce24053c
Lukoid <lucasvillela.unb@gmail.com>
2017-02-02 11:18:49 -0200

    Add files via upload

---
+++ vhdl/PC.vhd
@@ -0,0 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity PC is
+
+	port(
+      clk, rst: in std_logic;
+		D: in signed(31 downto 0);
+		Q: out signed(31 downto 0)
+	);
+end PC;
+
+
+architecture behavior of PC is
+
+   signal address: signed(31 downto 0);
+
+	begin
+
+		process(clk, rst)
+
+		begin
+			if(rst = '1') then
+
+				address <= "00000000000000000000000000000000";
+
+			else
+				if rising_edge(clk) then
+
+					address <= D;
+					
+				end if;
+			end if;
+		end process;
+		Q <= address;
+end behavior;
---
+++ vhdl/adder.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Adder is
+	port(
+		A, B: in signed(31 downto 0);
+		result: out signed(31 downto 0)
+	);
+end Adder;
+
+
+architecture behavior of Adder is
+begin
+    result <= A + B;
+end behavior;
